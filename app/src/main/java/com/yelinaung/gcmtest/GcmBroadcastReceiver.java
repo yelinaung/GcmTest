308f02475a613557901f7550103cc594d74b9f96
Ye Lin Aung <me@yelinaung.com>
2014-09-15 19:46:33 +0630

    gcm yo!

---
+++ app/src/main/java/com/yelinaung/gcmtest/GcmBroadcastReceiver.java
@@ -0,0 +1,20 @@
+package com.yelinaung.gcmtest;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+/**
+ * Created by Ye Lin Aung on 14/09/15.
+ */
+public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    ComponentName comp = new ComponentName(context.getPackageName(),
+        GCMNotificationIntentService.class.getName());
+    startWakefulService(context, (intent.setComponent(comp)));
+    setResultCode(Activity.RESULT_OK);
+  }
+}
---
+++ app/src/main/java/com/yelinaung/gcmtest/MyActivity.java
@@ -0,0 +1,89 @@
+package com.yelinaung.gcmtest;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import java.io.IOException;
+
+public class MyActivity extends Activity {
+
+  private GoogleCloudMessaging gcm;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_my);
+
+    gcm = GoogleCloudMessaging.getInstance(this);
+  }
+
+  @Override protected void onResume() {
+    super.onResume();
+    new RegisterGCMTask().execute();
+  }
+
+  private class RegisterGCMTask extends AsyncTask<Void, Void, String> {
+
+    @Override protected void onPreExecute() {
+      super.onPreExecute();
+      Log.i("pre", "registering ..");
+    }
+
+    @Override protected String doInBackground(Void... params) {
+      String msg;
+      try {
+        if (gcm == null) {
+          gcm = GoogleCloudMessaging.getInstance(getBaseContext());
+        }
+        String SENDER_ID = "592383644034";
+        String regId = gcm.register(SENDER_ID);
+        msg = "Device registered, registration ID=" + regId;
+
+        // For this demo: we don't need to send it because the device
+        // will send upstream messages to a server that echo back the
+        // message using the 'from' address in the message.
+
+        // Persist the regID - no need to register again.
+      } catch (IOException ex) {
+        msg = "Error :" + ex.getMessage();
+        // If there is an error, don't just keep trying to register.
+        // Require the user to click a button again, or perform
+        // exponential back-off.
+      }
+      return msg;
+    }
+
+    @Override protected void onPostExecute(String s) {
+      super.onPostExecute(s);
+      if (s != null) {
+        Log.i("GCM", s);
+      }
+    }
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.my, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+}
